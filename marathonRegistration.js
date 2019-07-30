f59871f24c2539136b045918cc2c277cec4fe661
nedralovesme <ardenrose@gmail.com>
2019-07-30 14:06:24 -0500

    refactor

--- marathonRegistration.js
+++ marathonRegistration.js
@@ -2,15 +2,15 @@ let raceNumber = Math.floor(Math.random() * 1000);
 let regEarly = false;
 let runnerAge = 18;
 
-if (runnerAge > 18 && regEarly === true){
+if (runnerAge > 18 && regEarly){
   raceNumber += 1000;
 };
 console.log(raceNumber);
 
-if (runnerAge > 18 && regEarly === true){
+if (runnerAge > 18 && regEarly){
   console.log(`Runner number ${raceNumber}, your race begins at 9:30am.`);
 }
-else if (runnerAge > 18 && regEarly === false){
+else if (runnerAge > 18 && !regEarly){
   console.log(`Runner number ${raceNumber}, your race begins at 11:00am.`);
 }
 else if (runnerAge < 18){
